78a729e4f5949f6cad6a5c558e0ad7960cf175fb
flutternet <163728706+flutternet@users.noreply.github.com>
2024-03-23 06:17:23 -0700

    Add files via upload

---
+++ new.js
@@ -0,0 +1,22 @@
+//Comparison Operators(boolean)
+var a = 10;
+var b = 20;
+console.log(a < b);
+var c = 40;
+var d = 30;
+console.log(c > d);
+var e = 50;
+var f = 60;
+console.log(e <= f);
+var g = 80;
+var h = 70;
+console.log(g >= h);
+var i = 90;
+var j = 90;
+console.log(i == j);
+var k = 100;
+var l = 110;
+console.log(k != l);
+var m = 120;
+var n = 120;
+console.log(m === n);
---
+++ new.ts
@@ -0,0 +1,25 @@
+//Comparison Operators(boolean)
+let a=10;
+let b=20;
+console.log(a<b)
+let c=40;
+let d=30;
+console.log(c>d)
+let e=50;
+let f=60;
+console.log(e<=f)
+let g=80;
+let h=70;
+console.log(g>=h)
+let i=90;
+let j=90;
+console.log(i==j)
+let k=100;
+let l=110
+console.log(k!=l)
+let m=120;
+let n=120;
+console.log(m===n)
+
+
+
